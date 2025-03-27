2cc1574b2caf67edec09a981f8bc1184899e0dce
doshamine <sdmarch213@gmail.com>
2025-03-27 17:49:17 +0600

    First commit

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'stock', 'product', 'quantity', 'price',]
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        stock = super().create(validated_data)
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        for position in positions:
+            pos_validated_data = {**position, 'stock': stock}
+            ProductPositionSerializer().create(pos_validated_data)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        for position in positions:
+            pos_validated_data = {**position, 'stock': stock}
+            pos_instance = StockProduct(**pos_validated_data)
+            ProductPositionSerializer().update(pos_instance, pos_validated_data)
+
+        return stock
